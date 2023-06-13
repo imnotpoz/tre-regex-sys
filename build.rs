41948245c49945b172e6a494dd0f3040cedce80c
Elizabeth Myers <elizabeth.jennifer.myers@gmail.com>
2023-06-12 23:38:33 -0700

    Use cheesiest hack known to man to build.

--- build.rs
+++ build.rs
@@ -1,5 +1,6 @@
 extern crate autotools;
 extern crate bindgen;
+extern crate fs_extra;
 
 use std::env;
 use std::path::PathBuf;
@@ -7,25 +8,50 @@ use std::process::Command;
 
 use autotools::Config;
 
-fn main() {
-    Command::new("sh")
-        .args(["-c", "cd tre && ./utils/autogen.sh"])
-        .status()
-        .expect("Could not run autogen.sh! Do you have autotools installed?");
+use fs_extra::dir::{copy, remove, CopyOptions};
 
-    let dst = Config::new("tre")
+fn main() {
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    let tre_path = out_path.join("tre");
+
+    // Clean up if we have to
+    remove(&tre_path).ok();
+
+    // Sigh, so tre does weird build stuff and we can't "just" run configure.
+    // We have to run the generation script, but we aren't supposed to modify outside OUT_DIR, so,
+    // we just copy it to out dir. Disgusting, I know. If you have a better option, please let me
+    // know.
+    let options = CopyOptions::new();
+    copy("tre", &out_path, &options).expect("Failed to copy tre!");
+
+    // Generate the config script.
+    // I hate autotools so much.
+    let script_path = tre_path.join("utils").join("autogen.sh");
+    let script_path = script_path.to_str().unwrap();
+    Command::new("sh")
+        .current_dir(&tre_path)
+        .args(["-c", script_path])
+        .status()
+        .expect("Could not run autogen.sh! Is autotools installed?");
+
+    // Now do the actual configure/build stuff
+    let dst = Config::new(out_path.join("tre"))
         .enable("static", None)
         .disable("shared", None)
         .disable("agrep", None)
         .build();
 
-    println!("cargo:rustc-link-search=native={}", dst.join("lib").display());
+    // Clean up our mess
+    remove(tre_path).expect("Could not clean up tre dir!");
+
+    println!(
+        "cargo:rustc-link-search=native={}",
+        dst.join("lib").display()
+    );
     println!("cargo:rustc-link-lib=static=tre");
     println!("cargo:rustc-link-lib=c");
-    
-    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
-    let include_path = out_path.join("include");
 
+    let include_path = out_path.join("include");
     let bindings = bindgen::Builder::default()
         .header(include_path.join("tre.h").to_str().unwrap())
         .newtype_enum("reg_errcode_t")
--- src/lib.rs
+++ src/lib.rs
@@ -6,22 +6,34 @@ include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
 #[cfg(test)]
 mod tests {
-    use std::mem;
     use super::*;
+    use std::mem;
 
     #[test]
     fn regcomp() {
         unsafe {
             let mut preg = mem::MaybeUninit::<regex_t>::uninit();
-            if tre_regcomp(preg.as_mut_ptr(), b"Hello(, [[:alpha:]]+)?!\0".as_ptr() as * const _, (REG_EXTENDED | REG_ICASE) as i32) != 0 {
+            if tre_regcomp(
+                preg.as_mut_ptr(),
+                b"Hello(, [[:alpha:]]+)?!\0".as_ptr() as *const _,
+                (REG_EXTENDED | REG_ICASE) as i32,
+            ) != 0
+            {
                 panic!("tre_regcomp");
             }
 
             let preg = preg.assume_init();
-            
+
             let nmatch = 1;
             let mut pmatch: Vec<regmatch_t> = vec![regmatch_t { rm_so: 0, rm_eo: 0 }; 1];
-            if tre_regexec(&preg, b"Hello!".as_ptr() as * const _, nmatch, pmatch.as_mut_ptr(), 0) != 0 {
+            if tre_regexec(
+                &preg,
+                b"Hello!".as_ptr() as *const _,
+                nmatch,
+                pmatch.as_mut_ptr(),
+                0,
+            ) != 0
+            {
                 panic!("tre_regexec");
             }
 
@@ -32,7 +44,14 @@ mod tests {
 
             let nmatch = 2;
             pmatch.push(regmatch_t { rm_so: 0, rm_eo: 0 });
-            if tre_regexec(&preg, b"Hello, world!".as_ptr() as * const _, nmatch, pmatch.as_mut_ptr(), 0) != 0 {
+            if tre_regexec(
+                &preg,
+                b"Hello, world!".as_ptr() as *const _,
+                nmatch,
+                pmatch.as_mut_ptr(),
+                0,
+            ) != 0
+            {
                 panic!("tre_regexec");
             }
 
