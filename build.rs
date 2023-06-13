449ad17f57d2906817123ca9f6e17f1db6ebf95b
Elizabeth Myers <elizabeth.jennifer.myers@gmail.com>
2023-06-12 22:14:10 -0700

    Misc build fixes

--- build.rs
+++ build.rs
@@ -9,7 +9,6 @@ use autotools::Config;
 fn main() {
     let dst = Config::new("tre")
         .reconf("-ivf")
-        .insource(true)
         .enable("static", None)
         .disable("shared", None)
         .disable("agrep", None)
@@ -18,9 +17,12 @@ fn main() {
     println!("cargo:rustc-link-search=native={}", dst.join("lib").display());
     println!("cargo:rustc-link-lib=static=tre");
     println!("cargo:rustc-link-lib=c");
+    
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    let include_path = out_path.join("include");
 
     let bindings = bindgen::Builder::default()
-        .header("tre/include/tre.h")
+        .header(include_path.join("tre.h").to_str().unwrap())
         .newtype_enum("reg_errcode_t")
         .allowlist_function("tre_.*")
         .allowlist_type("(reg.*_t|tre_.*)")
@@ -29,7 +31,6 @@ fn main() {
         .generate()
         .expect("Unable to generate bindings");
 
-    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
     bindings
         .write_to_file(out_path.join("bindings.rs"))
         .expect("Couldn't write bindings!");
